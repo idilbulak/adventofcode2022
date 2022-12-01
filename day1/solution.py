b9fb491a74b249e7b2217361c650ff8c01b13f90
idil bulak <ib@idils-MacBook-Pro-2.local>
2022-12-01 20:54:01 +0100

    ok

---
+++ day1/solution.py
@@ -0,0 +1,15 @@
+with open('calories.txt') as file:
+    list = []
+    for line in file:
+        list.append(line.strip())
+
+calories = []
+elves = (','.join(list)).split(',,')
+for elf in elves:
+    totalcal = 0
+    for x in elf.split(','):
+        totalcal += int(x)
+    calories.append(totalcal)
+
+calories.sort(reverse=True)
+print(max(calories), sum(calories[:3]))
